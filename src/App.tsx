9711159c7729716720dcf0c9acff1da6e3d11ca7
Carlos Olcina <147743891+carlosOlcina@users.noreply.github.com>
2026-07-08 10:11:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div>
+          <p>
+            Si intercambiáramos cuerpos por un día, ¿qué sería lo primero que
+            harías?
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
